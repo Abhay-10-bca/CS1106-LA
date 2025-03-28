964527178b425fd4fe1faaa597d09c38144dd099
Abhay-10-bca <abhaymbijubca24@rvu.edu.in>
2025-03-28 09:43:55 +0530

    Update Activity4LA.sh

--- Lab6/Activity4LA.sh
+++ Lab6/Activity4LA.sh
@@ -3,6 +3,6 @@ cp *.c /tmp/Backup
 cp *.py /tmp/Backup
 cd /tmp
 tar -czvf Backup-2025-03-28.tar.gz Backup
-udisksctl mount -b /dev/sdA1
+udisksctl mount -b /dev/sda1
 mv /tmp/Backup-2025-03-28.tar.gz /media/RVU/Pendrive/
-udisksctl unmount -b /dev/sdA1
+udisksctl unmount -b /dev/sda1
