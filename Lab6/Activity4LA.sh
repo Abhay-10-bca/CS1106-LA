0e76b449d6b50d9780881703e798a8e90acd34e6
Abhay-10-bca <abhaymbijubca24@rvu.edu.in>
2025-03-28 09:40:42 +0530

    Create Activity4LA.sh

---
+++ Lab6/Activity4LA.sh
@@ -0,0 +1,8 @@
+mkdir /tmp/Backup
+cp *.c /tmp/Backup
+cp *.py /tmp/Backup
+cd /tmp
+tar -czvf Backup-2025-03-28.tar.gz Backup
+udisksctl mount -b /dev/sdA1
+mv /tmp/Backup-2025-03-28.tar.gz /media/$USER/PENDRIVE/
+udisksctl unmount -b /dev/sdA1
